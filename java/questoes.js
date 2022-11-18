c59736697a4d46d3b6b95c43c72a06ab1804aacc
EmmanuelD1 <118396740+EmmanuelDinz1@users.noreply.github.com>
2022-11-18 01:04:59 -0300

    Add files via upload

---
+++ java/questoes.js
@@ -0,0 +1,65 @@
+// manipulação
+
+const pessoas = [
+    { name: "Fabiana Araújo", age: 33 },
+    { name: "Gabriel Gomes", age: 25 },
+    { name: "Fernando Henrique", age: 17 },
+    { name: "Ana Luiza", age: 2 }, 
+    { name: "Geralda do Nascimento", age: 93 },
+    { name: "Miguel Souza", age: 70 },
+    { name: "Antonio Miguel", age: 69 }
+];
+function exibirNomes(){
+    const todos = pessoas.map(item =>{
+        console.log(item.name + pessoas.age)})
+}
+
+
+// teste 3 -- retorne os dados de uma pessoa com um array
+
+function dadosPessoa(name){
+    const nomePessoa = pessoas.map(item => item.name == name);
+    return item + item.age;
+    }console.log(dadosPessoa("Gabriel Gomes"))
+
+
+// teste 4 -- criar uma função que retorne um vetor com todos nomes das pessoas
+function vetorDeNomes(){
+    const nomes = pessoas.map(item => item.name.split(" ")[0]);
+    return nomes;
+    }console.log(vetorNomes())
+
+
+// teste 5 -- criar uma função para inserir um id único para cada pessoa no vetor
+
+function adicionarId(){
+    let cont = 1
+    const add = pessoas.map(item => {
+        pessoas.id = contador
+    contador ++});
+}
+console.log(item)
+
+
+// teste 6 -- criar função para retornar os dados das pessoas que tem idade insuficente para sua primeira habilitação
+function habilitados(){
+const habilitação = pessoas.filter(maior => maior.age >= 18);
+return habilitação;
+console.log(habilitados())}
+
+
+// teste 7 - criar uma função para retornar a media das idades das pessoas
+
+
+function calcularMedia(){
+    var som = 0;
+    for (i = 0; i< pessoas.length; i++){
+    som += pessoas[i].age;}
+        
+const media = som / pessoas.length
+
+}
+console.log(calcularMedia())
+
+
+    
